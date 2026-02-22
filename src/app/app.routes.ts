7bea69f90e87367fba92d165361a2191629d8f7c
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2026-02-23 02:24:18 +0530

    add login UI with form validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login-component/login-component';
+import { ProductListComponent } from './components/product-list.component/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductListComponent }
+];
