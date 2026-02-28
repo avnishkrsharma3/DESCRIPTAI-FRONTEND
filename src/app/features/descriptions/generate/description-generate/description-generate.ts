6f3e190647775cd5eaa9313302104ef52a9a3532
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2026-03-01 01:05:12 +0530

    add live description preview with debouncing

--- src/app/features/descriptions/generate/description-generate/description-generate.ts
+++ src/app/features/descriptions/generate/description-generate/description-generate.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { debounce } from '@angular/forms/signals';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-description-generate',
@@ -12,11 +14,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class DescriptionGenerate {
 
   products: number[] = [];
-  tones = ['professional', 'casual', 'persuasive'];
-  focusOptions = ['seo', 'conversion', 'infomative'];
-  lengths = ['short', 'medium', 'long'];
+  tones = ['Professional', 'Casual', 'Persuasive'];
+  focusOptions = ['Seo', 'Conversion', 'Infomative'];
+  lengths = ['Short', 'Medium', 'Long'];
   size: number = 0;
   form!: FormGroup;
+  preview!: string;
+  isTyping: boolean = false;
 
   ngOnInit(): void {
     const state = history.state;
@@ -26,9 +30,13 @@ export class DescriptionGenerate {
       return
     this.products = state.productIds;
     this.form = this.fb.group({
-      tone: ['professional', Validators.required],
-      length: ['medium', Validators.required],
-      focus: ['seo', Validators.required]
+      tone: ['Professional', Validators.required],
+      length: ['Medium', Validators.required],
+      focus: ['Seo', Validators.required]
+    });
+    this.generatePreview();
+    this.form.valueChanges.pipe(debounceTime(0)).subscribe(() => {
+      this.generatePreview();
     });
   }
 
@@ -37,7 +45,45 @@ export class DescriptionGenerate {
   }
 
   onGenerate() {
+    console.log(this.products[0])
     console.log(this.form.value);
+  }
+
+
+  // product id 2 description will get generated based on tone Professional , length medium and focus seo.
+  generatePreview() {
+    if (!this.products.length) return;
+
+    const { tone, length, focus } = this.form.value;
+
+    const fullText =
+      "Product id " +
+      this.products[0] +
+      " description will get generated based on tone " +
+      tone +
+      ", length " +
+      length +
+      " and focus " +
+      focus + ".";
+
+    this.simulateTyping(fullText);
+  }
+
+  simulateTyping(text: string) {
+    this.preview = '';
+    this.isTyping = true;
+
+    let index = 0;
+
+    const interval = setInterval(() => {
+      this.preview += text[index];
+      index++;
+
+      if (index >= text.length) {
+        clearInterval(interval);
+        this.isTyping = false;
+      }
+    }, 10);
   }
 
 }
