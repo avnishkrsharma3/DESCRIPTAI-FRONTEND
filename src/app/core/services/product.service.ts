860fd992b7a4b5241a8d3d6225cc551ea240a156
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2026-03-14 23:58:27 +0530

    integrated basic api's with backend

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -2,23 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product, ProductsResponse } from '../models/product.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'https://dummyjson.com/products';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   // Get all products
   getAllProducts(limit: number = 100): Observable<ProductsResponse> {
-    return this.http.get<ProductsResponse>(`${this.apiUrl}?limit=${limit}`);
+    return this.http.get<ProductsResponse>(`${this.apiUrl}`);
   }
 
   // Search for products
   searchProducts(query: string): Observable<ProductsResponse> {
     return this.http.get<ProductsResponse>(`${this.apiUrl}/search?q=${query}`);
   }
- 
+
 }
